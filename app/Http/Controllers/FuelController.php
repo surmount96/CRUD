ce14bc5eeab61da2c67e0ece66f404aebec5cd19
Ayodeji <ayanwoye74@gmail.com>
2019-06-11 13:51:41 +0100

    real

--- app/Http/Controllers/FuelController.php
+++ app/Http/Controllers/FuelController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests;
-use App\fuel;
+use App\Fuel;
 use App\Http\Resources\Fuel as FuelResource;
 
 class FuelController extends Controller
